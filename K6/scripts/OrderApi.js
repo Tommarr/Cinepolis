731d9f7e8f7b0007471aa3b8e199fdcbe28af611
Tommarr <tommarrewijk@gmail.com>
2023-04-30 13:21:40 +0200

    Added k6 load testing

---
+++ K6/scripts/OrderApi.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+import { check, sleep } from "k6";
+
+export let options = {
+  stages: [
+      // Ramp-up from 1 to TARGET_VUS virtual users (VUs) in 5s
+      { duration: "5s", target: 100 },
+
+      // Stay at rest on TARGET_VUS VUs for 10s
+      { duration: "10s", target: 50000 },
+
+      // Ramp-down from TARGET_VUS to 0 VUs for 5s
+      { duration: "5s", target: 0 }
+  ]
+};
+
+export default function () {
+  const response = http.get("https://host.docker.internal:55003/api/Order", {headers: {Accepts: "*/*"}});
+  check(response, { "status is 200": (r) => r.status === 200 });
+  sleep(.300);
+};
